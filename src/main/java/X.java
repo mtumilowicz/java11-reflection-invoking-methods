b58a46dfa6b229ae4d11b83322167e90a0dfcea8
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-04 23:23:30 +0100

    class + tests

---
+++ src/main/java/X.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mtumilowicz on 2019-02-04.
+ */
+class X {
+    String concat(String string, int i) {
+        return string + i;
+    }
+}
---
+++ src/test/java/MethodInvokerTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by mtumilowicz on 2019-02-04.
+ */
+public class MethodInvokerTest {
+    @Test
+    public void invoke() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        var concatenated = (String) X.class.getDeclaredMethod("concat", String.class, int.class)
+                .invoke(new X(),"a", 1);
+        
+        assertThat(concatenated, is("a1"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invoke_wrongArguments() throws NoSuchMethodException,
+            IllegalAccessException,
+            InvocationTargetException {
+        X.class.getDeclaredMethod("concat", String.class, int.class).invoke("a", "a", "b");
+    }
+}
